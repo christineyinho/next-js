06a40a563b2e07cc7d56a54efa02b4eb22bc27af
Christine <christineyinho@gmail.com>
2021-07-09 17:46:35 +1000

    add sections, add info

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -1,43 +1,33 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from './layout.module.css'
+import { Header } from './header';
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Christine Yin Ho'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Christine Yin Ho Portfolio'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
+      <Header />
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Christine Yin Ho Portfolio"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content=""
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
         {home ? (
-          <>
-            <Image
-              priority
-              src="/images/christineyinho-github.jpeg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
         ) : (
           <>
             <Link href="/">
@@ -60,7 +50,7 @@ export default function Layout({ children, home }) {
           </>
         )}
       </header>
-      <main>{children}</main>
+      <main className={styles.mainContainer}>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">
--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,6 +3,9 @@ import Link from 'next/link'
 
 import Layout, { siteTitle } from '../components/layout'
 import Date from '../components/date'
+import { StickyNav } from '../components/stickyNav';
+import { WorkInformation } from '../components/workInformation';
+import { Contact } from "../components/contact";
 
 import { getSortedPostsData } from '../lib/posts'
 
@@ -24,26 +27,42 @@ const Home = ({
     <Head>
       <title>{siteTitle}</title>
     </Head>
-    <section className={utilStyles.headingMd}>
-      <p>Hi &#128075; I'm Christine. Currently a Front End Engineer at <a href="https://chuffed.org">Chuffed.org</a>. A keen foodie, book worm, walker and interest in fashion.
-      </p>
-    </section>
-    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-      <h2 className={utilStyles.headingLg}>Blog</h2>
-      <ul className={utilStyles.list}>
-        {allPostsData.map(({ id, date, title }) => (
-          <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <div className={utilStyles.section}>
+      <StickyNav />
+    </div>
+    <div className={utilStyles.section} id="about">
+      <section className={utilStyles.headingMd}>
+        <p>Hi &#128075; I'm Christine. Currently a Front End Engineer at <a href="https://chuffed.org">Chuffed.org</a>. A keen foodie, book worm, walker and interest in fashion.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Duis libero purus, volutpat imperdiet aliquam ac, vulputate sit amet ex.
+          Integer ac eleifend nibh, id rhoncus nibh.
+          Cras nisl purus, lacinia a consequat ut, tristique pellentesque dui.
+          Sed at pharetra diam. Sed ultrices odio a nisi blandit, nec luctus felis pharetra.
+          Sed congue maximus velit et pretium.
+          Ed dui diam, volutpat nec ante non, tempus laoreet purus.
+          Donec lacinia volutpat sapien, sed pretium nibh volutpat vel.
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <WorkInformation />
+        <h2 id="blog" className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list} >
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+        <Contact />
+      </section>
+    </div>
   </Layout>
 );
 
